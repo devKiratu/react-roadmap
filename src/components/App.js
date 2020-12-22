6d35258a1f44b30474199fc9fdc329d25e96a709
devkiratu <dev.kiratu@gmail.com>
2020-12-22 11:50:20 +0300

    add a log in screen with basic styling in plain css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./Login";
 
 export default function App() {
+	const [userName, setUserName] = useState("");
+	const [pwd, setPwd] = useState("");
+
 	return (
 		<div>
-			<Login />
+			<Login
+				userName={userName}
+				setUserName={setUserName}
+				pwd={pwd}
+				setPwd={setPwd}
+			/>
 		</div>
 	);
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,32 @@
 import React from "react";
 
-export default function Login() {
-	return <div className="login-screen">Am the login screen</div>;
+export default function Login(props) {
+	const { userName, setUserName, pwd, setPwd } = props;
+	return (
+		<div className="login-screen">
+			<h3>Sign in to proceed</h3>
+			<form>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					placeholder="Enter username or email here"
+					name="username"
+					id="username"
+					value={userName}
+					onChange={(e) => setUserName(e.target.value)}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					placeholder="Enter password here"
+					name="password"
+					id="password"
+					value={pwd}
+					onChange={(e) => setPwd(e.target.value)}
+				/>
+				<button type="submit">Sign in</button>
+			</form>
+			<p className="copyright">©</p>
+		</div>
+	);
 }
