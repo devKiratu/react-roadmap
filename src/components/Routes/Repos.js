dacab0591473551599a80eb7863d9134717c7e21
devKiratu <dev.kiratu@gmail.com>
2022-12-17 10:00:14 +0300

    bugfix: utilize useEffect correctly

--- src/components/Routes/Repos.js
+++ src/components/Routes/Repos.js
@@ -5,50 +5,43 @@ import SearchForm from "../SearchForm";
 import NotFound from "./NotFound";
 
 export default function Repos() {
-	const [displayItems, setDisplayItems] = useState([]);
-	const [userName, setUserName] = useState("devkiratu");
-	const [error, setError] = useState("");
-	const [repoOwner, setRepoOwner] = useState("");
+  const [displayItems, setDisplayItems] = useState([]);
+  const [userName, setUserName] = useState("devkiratu");
+  const [error, setError] = useState("");
+  const [repoOwner, setRepoOwner] = useState("");
 
-	async function getItems() {
-		const url = `https://api.github.com/users/${userName}/repos`;
+  useEffect(() => {
+    async function getItems() {
+      const url = `https://api.github.com/users/${userName}/repos`;
 
-		const response = await fetch(url);
-		const data = await response.json();
-		data.message || data[0] === undefined
-			? setError("not found")
-			: setDisplayItems(data);
-		setRepoOwner(userName);
-		// console.log(data);
-	}
+      const response = await fetch(url);
+      const data = await response.json();
+      data.message || data[0] === undefined
+        ? setError("not found")
+        : setDisplayItems(data);
+      setRepoOwner(userName);
+      // console.log(data);
+    }
+    getItems();
+  }, [userName]);
 
-	useEffect(() => {
-		getItems();
-		setUserName("");
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	return (
-		<>
-			{error ? (
-				<NotFound />
-			) : (
-				<>
-					<Title>
-						Displaying public github repos for <code>{repoOwner}</code>
-					</Title>
-					<SearchForm
-						userName={userName}
-						setUserName={setUserName}
-						getItems={getItems}
-					/>
-					<StyledRepo>
-						{displayItems.map((displayItem) => (
-							<DisplayCard displayItem={displayItem} key={displayItem.id} />
-						))}
-					</StyledRepo>
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {error ? (
+        <NotFound />
+      ) : (
+        <>
+          <Title>
+            Displaying public github repos for <code>{repoOwner}</code>
+          </Title>
+          <SearchForm setUserName={setUserName} />
+          <StyledRepo>
+            {displayItems.map((displayItem) => (
+              <DisplayCard displayItem={displayItem} key={displayItem.id} />
+            ))}
+          </StyledRepo>
+        </>
+      )}
+    </>
+  );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-	StyledSearchForm,
-	SearchFormInput,
-	SearchFormButton,
-	SearchFormLabel,
+  StyledSearchForm,
+  SearchFormInput,
+  SearchFormButton,
+  SearchFormLabel,
 } from "../styles/Form";
 
-export default function SearchForm({ userName, setUserName, getItems }) {
-	function handleSubmit(e) {
-		e.preventDefault();
-		getItems();
-		setUserName("");
-	}
+export default function SearchForm({ setUserName }) {
+  const [query, setQuery] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUserName(query);
+    setQuery("");
+  }
 
-	return (
-		<StyledSearchForm onSubmit={(e) => handleSubmit(e)}>
-			<SearchFormLabel htmlFor="search-user">Search User</SearchFormLabel>
-			<SearchFormInput
-				type="text"
-				name="search-user"
-				placeholder="Search github"
-				value={userName}
-				onChange={(e) => setUserName(e.target.value)}
-			/>
-			<SearchFormButton>Search</SearchFormButton>
-		</StyledSearchForm>
-	);
+  return (
+    <StyledSearchForm onSubmit={(e) => handleSubmit(e)}>
+      <SearchFormLabel htmlFor="search-user">Search User</SearchFormLabel>
+      <SearchFormInput
+        type="text"
+        name="search-user"
+        placeholder="Search github"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <SearchFormButton>Search</SearchFormButton>
+    </StyledSearchForm>
+  );
 }
